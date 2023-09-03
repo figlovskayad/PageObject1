b5e59178b7c182649e4d732fb7b92c4bd4d33fb7
netol <figlovskayad@gmail.com>
2023-09-04 00:35:04 +0300

    4

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -3,7 +3,6 @@ package ru.netology.page;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.val;
-import org.jetbrains.annotations.NotNull;
 import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.*;
@@ -32,7 +31,7 @@ public class DashboardPage {
     }
 
 
-    private int extractBalance(@NotNull String text) {
+    private int extractBalance( String text) {
         val start = text.indexOf(balanceStart);
         val finish = text.indexOf(balanceFinish);
         val value = text.substring(start + balanceStart.length(), finish);
